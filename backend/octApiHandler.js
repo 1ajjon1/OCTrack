a89f0d1ba1183fcc3e8a8c518f2cc295357e16df
DJ-Watson <djimonwatson@gmail.com>
2025-03-23 20:49:20 -0400

    edited code so request is asynchronous

--- backend/octApiHandler.js
+++ backend/octApiHandler.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
+import GtfsRealtimeBindings from "gtfs-realtime-bindings";
 
 // endpoint + key
 const url = "https://nextrip-public-api.azure-api.net/octranspo/gtfs-rt-vp/beta/v1/VehiclePositions";
 const apiKey = process.env.API_KEY;
-const authHeader = {"Ocp-Apim-Subscription-Key":apiKey};
 
 // data fetch request message
 function httpRequest() {
-    var reqObj = new XMLHttpRequest();
-    reqObj.onreadystatechange = function() {
-        if (reqObj.readyState == 4 && reqObj.status == 200) {
-            // response
-        }
-    }
-    reqObj.setRequestHeader("Ocp-Apim-Subscription-Key", "Bearer " + apiKey);
+    const reqObj = new XMLHttpRequest();
+    reqObj.setRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
     reqObj.open("GET", url, true);
+    reqObj.responseType = "arraybuffer";
+    if (reqObj.readyState == 4 && reqObj.status == 200) {
+        console.log(reqObj.responseText);
+    } else {
+        console.error(reqObj.statusText);
+    }
     // reqObj.send();
 }
 
