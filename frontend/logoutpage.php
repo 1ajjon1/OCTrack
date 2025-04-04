9159972a329e8607290492ebb25c018837ce13e4
DJ-Watson <djimonwatson@gmail.com>
2025-04-04 02:54:50 -0400

    conversion of userpage back to php, added subpage php file, added scripting to change page on dropdown select (not working atm)

---
+++ frontend/logoutpage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="authors" content="Saravanan Rajeswaran; DJ Watson; Taleiven Goundan; Justin Le">
+        <script src="../backend/dbServer.js"></script>
+        <script src="../backend/apiHandler.js"></script>
+        <link rel="stylesheet" type="text/css" href="Stylesheet/Stylesheet_A1.css">
+        <link rel="preconnect" href="https://rsms.me/">
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    </head>
+    <body>
+        Placeholder logout
+    </body>
+</html>
